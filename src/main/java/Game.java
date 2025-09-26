10d942385ca0dc4791d5a5276849b6eec9283763
smokethecookie <ackura@bu.edu>
2025-09-26 18:52:28 -0500

    Player Class || players array in Game || integrate into SliderGame

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -13,6 +13,9 @@ public abstract class Game {
     public int board_height;
     public int turn_count=0;
 
+    // Shared players array (1+ players depending on the game)
+    protected Player[] players;
+
     public static String RESERVED_QUIT_KEYWORD = "QUIT";
     public static Scanner scanner = new Scanner(System.in);
 
@@ -21,6 +24,10 @@ public abstract class Game {
         board_width = width;
         board_height = height;
     }
+
+    // set/get for players array
+    protected void setPlayers(Player[] players) { this.players = players; }
+    protected Player[] getPlayers() { return this.players; }
 
     protected String getHowToQuitMessage(){
         return "You can quit anytime by entering 'QUIT' as your input for any question";
