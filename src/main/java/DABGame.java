a9294d8f376691533d63fa890d1a4580004b75a3
Owen Mariani <mariani.owen@gmail.com>
2025-10-03 11:35:54 -0700

    skeleton of DABGame created

--- src/main/java/DABGame.java
+++ src/main/java/DABGame.java
@@ -1,6 +1,15 @@
-//public class DABGame extends Game{
-//
-//    public DABGame(int width, int height){
-//        super(width, height);
-//    }
-//}
+public class DABGame extends Game{
+
+    public DABGame(int width, int height){
+        super(width, height);
+    }
+
+    public void playGame(){}
+
+    public void executeNextMove(){}
+
+    public boolean checkWin(){return false;}
+
+    public void endGame(){}
+
+}
