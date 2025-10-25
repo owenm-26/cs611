5616385ad43b07f5f82fcf15f852f70a8d2e59c6
Owen Mariani <mariani.owen@gmail.com>
2025-10-24 23:15:10 -0400

    made the y coordinate input as letters

--- src/main/java/edu/bu/cs611/quoridor/Position.java
+++ src/main/java/edu/bu/cs611/quoridor/Position.java
@@ -1,2 +1,15 @@
-package edu.bu.cs611.quoridor;public class Position {
+package edu.bu.cs611.quoridor;
+
+import java.util.Objects;
+
+public class Position {
+    int r, c;
+    public Position(int r, int c) { this.r = r; this.c = c; }
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+        Position p = (Position) o;
+        return r == p.r && c == p.c;
+    }
+    @Override public int hashCode() { return Objects.hash(r, c); }
 }
